fed8319573aa59465ca1622d123edcc9b12caa5a
Cledson Francisco Silva <70558441+cledsonfs-cmd@users.noreply.github.com>
2024-05-24 16:31:34 -0300

    24-05-2024-1

---
+++ automated-tests-with-java-erudio/src/test/java/br/com/erudio/business/CourseBusenessMockWithBDDTest.java
@@ -0,0 +1,52 @@
+package br.com.erudio.business;
+
+import br.com.erudio.service.CourseService;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matcher.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CourseBusenessMockWithBDDTest {
+    CourseService mockService;
+    CourseBuseness buseness ;
+    List<String> courses;
+
+    @BeforeEach
+    void setup(){
+        //Given
+        mockService = Mockito.mock(CourseService.class);
+        buseness = new CourseBuseness(mockService);
+        courses = Arrays.asList(
+                "REST API's RESTFul do 0 à Azure com ASP.NET Core 5 e Docker",
+                "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+                "Spotify Engineering Culture Desmistificado",
+                "REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker",
+                "Docker do Zero à Maestria - Contêinerização Desmistificada",
+                "Docker para Amazon AWS Implante Apps Java e .NET com Travis CI",
+                "Microsserviços do 0 com Spring Cloud, Spring Boot e Docker",
+                "Arquitetura de Microsserviços do 0 com ASP.NET, .NET 6 e C#",
+                "REST API's RESTFul do 0 à AWS com Spring Boot 3 Kotlin e Docker",
+                "Kotlin para DEV's Java: Aprenda a Linguagem Padrão do Android",
+                "Microsserviços do 0 com Spring Cloud, Kotlin e Docker"
+        );
+    }
+    @Test
+    void testCoursesRelatedToSpring_When_UsingAMock(){
+        //Give
+        BDDMockito.given(mockService.retriveCurses("Leandro")).willReturn(courses);
+
+        //When
+        var filteredCourses = buseness.retriveCousesRelatedRoSpring("Leandro");
+
+        //Then
+        MatcherAssert.assertThat(filteredCourses.size(), is(4));
+    }
+
+}
